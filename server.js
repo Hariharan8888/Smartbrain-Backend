dccb92935badc70f63874975bcc3ad36b7ea2536
K.Hariharan <jadeghari08@gmail.com>
2023-07-05 09:45:47 -0700

    nn

--- server.js
+++ server.js
@@ -12,13 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl : { rejectUnauthorized: false },
-    host : process.env.DATABASE_HOST,
-    user : process.env.DATABASE_USER,
-    port: 5432,
-    password : process.env.DATABASE_PW,
-    database : process.env.DATABASE_DB
+    host: process.env.DATABASE_URL,
+    ssl : true,
   }
 });
 
